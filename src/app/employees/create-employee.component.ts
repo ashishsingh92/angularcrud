9ce6c96a115d691934a38864d16de5b71dfedd96
Ashish Singh <si.ashish92@gmail.com>
2018-05-20 03:26:36 +0530

    Initial commit

---
+++ src/app/employees/create-employee.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Department } from '../models/department.model';
+import { Employee} from '../models/employee.model';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.css']
+})
+export class CreateEmployeeComponent implements OnInit {
+  gender = 'female';
+  department= '3';
+  dateOfBirth: Date = new Date(2018, 0, 30);
+  previewPhoto = false;
+  employee: Employee = {
+    id: null,
+    name: null,
+    gender: null,
+    contactPreference: null,
+    phoneNumber: null,
+    email: '',
+    dateOfBirth: null,
+    department: '-1',
+    isActive: null,
+    photoPath: null
+  };
+
+  departments: Department[]= [
+    { id: 1, name: 'Help Desk' },
+    { id: 2, name: 'HR' },
+    { id: 3, name: 'IT' },
+    { id: 4, name: 'Payroll' }
+  ];
+
+  constructor() { }
+
+  togglePhotoPreview() {
+    this.previewPhoto = !this.previewPhoto;
+  }
+
+  ngOnInit() {
+  }
+
+  saveEmployee(newEmployee: Employee): void {
+    console.log(newEmployee);
+  }
+}
